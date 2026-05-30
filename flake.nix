1c6fc4e45fb36a78947c3a5d86e081bdf6bdb084
lumahere <sum1hewe.dev@gmail.com>
2026-05-30 10:56:48 +0800

    moved gnome to folder

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Single host NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05-beta";
     home-manager = {
-          url = "github:nix-community/home-manager";
-          inputs.nixpkgs.follows = "nixpkgs"; # Keep versions synced
-        };
-        stylix.url = "github:danth/stylix";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs"; # Keep versions synced
+    };
+    stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
@@ -18,12 +18,12 @@
         ./hosts/laptop/configuration.nix
         stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
-                {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.extraSpecialArgs = { inherit inputs; };
-                  home-manager.users.nael = import ./hosts/laptop/home.nix;
-                }
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.nael = import ./hosts/laptop/home.nix;
+        }
       ];
     };
   };
--- modules/apps/virtualization.nix
+++ modules/apps/virtualization.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
 
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
-  };
-  programs.virt-manager.enable = true;
-  users.users."nael".extraGroups = [ "libvirtd" ];
+  virtualisation.virtualbox.host.enableKvm = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enable = true;
+
+  virtualisation.virtualbox.host.addNetworkInterface = false;
+  users.users."nael".extraGroups = [ "libvirtd" "vboxusers" ];
 
   environment.systemPackages = with pkgs; [
     dnsmasq
